36985b5d5767298be8a1889f900619e218891a34
Max <maxim.ionko@gmail.com>
2020-11-17 23:17:46 +0300

    treads

--- Third/ACS_HW3.cpp
+++ Third/ACS_HW3.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <thread>
 
 using namespace std;
 
@@ -35,7 +36,7 @@ void checkUnion(vector<int> A, vector<int> B, vector<int> C) {
 	}
 	else
 	{
-		cout << "C not is union of A and B" << endl;
+		cout << "C is not union of A and B" << endl;
 	}
 }
 
@@ -106,6 +107,7 @@ void checkResidualAB(vector<int> A, vector<int> B, vector<int> C) {
 		cout << "C is not A/B" << endl;
 	}
 }
+
 void checkResidualBA(vector<int> B, vector<int> A, vector<int> C) {
 	set<int> s;
 	vector <int> v_intersection;
@@ -139,48 +141,17 @@ void checkResidualBA(vector<int> B, vector<int> A, vector<int> C) {
 		cout << "C is not B/A" << endl;
 	}
 }
+
 int main()
 {
-	/* int n;
-	 string input;
-
-	 cout << "Enter length of A" << endl;
-	 cin >> n;
-	 int* A = new int[n];
-
-	 cout << "Enter numbers for A" << endl;
-	 cin >> input;
-	 for (int i = 0; i < input.length(); i+=2)
-	 {
-		 A[i/2] = input[i];
-	 }
-
-	 cout << "Enter length of B" << endl;
-	 cin >> n;
-	 int* B = new int[n];
-
-	 cout << "Enter numbers for B" << endl;
-	 cin >> input;
-	 for (int i = 0; i < input.length(); i += 2)
-	 {
-		 B[i / 2] = input[i];
-	 }
-
-	 cout << "Enter length of C" << endl;
-	 cin >> n;
-	 int* C = new int[n];
-
-	 cout << "Enter numbers for C" << endl;
-	 cin >> input;
-	 for (int i = 0; i < input.length(); i += 2)
-	 {
-		 C[i / 2] = input[i];
-	 }*/
+	vector<int> A;
+	vector<int> B;
+	vector<int> C;
 
 	//C is a union of A and B
-	vector<int> A = { 1, 3, 3, 4 };
+	/*vector<int> A = { 1, 3, 3, 4 };
 	vector<int> B = { 1, 3, 3 };
-	vector<int> C = { 1, 3, 4 };
+	vector<int> C = { 1, 3, 4 };*/
 
 	//C is an intersection
 	/*vector<int> A = { 1, 3, 3, 4 };
@@ -197,46 +168,44 @@ int main()
 	vector<int> B = { 1, 3, 3, 4 };
 	vector<int> C = { 4 };*/
 
-	//int sizeofA = rand() % 5 + 1;
-	//int sizeofB = rand() % 5 + 1;
-	//int sizeofC = rand() % 5 + 1;
+	int sizeofA = rand() % 5 + 1;
+	int sizeofB = rand() % 5 + 1;
+	int sizeofC = rand() % 5 + 1;
 
-	//cout << "A: ";
-	//for (int i = 0; i < sizeofA; i++)
-	//{
-	//	A.push_back(rand() % 10 + 1);
-	//	cout << A[i] << " ";
-	//}
+	cout << "A: ";
+	for (int i = 0; i < sizeofA; i++)
+	{
+		A.push_back(rand() % 10 + 1);
+		cout << A[i] << " ";
+	}
 
-	//cout << endl << "B: ";
-	//for (int i = 0; i < sizeofB; i++)
-	//{
-	//	B.push_back(rand() % 10 + 1);
-	//	cout << B[i] << " ";
-	//}
+	cout << endl << "B: ";
+	for (int i = 0; i < sizeofB; i++)
+	{
+		B.push_back(rand() % 10 + 1);
+		cout << B[i] << " ";
+	}
 
-	//cout << endl << "C: ";
-	//for (int i = 0; i < sizeofC; i++)
-	//{
-	//	C.push_back(rand() % 10 + 1);
-	//	cout << C[i] << " ";
-	//}
+	cout << endl << "C: ";
+	for (int i = 0; i < sizeofC; i++)
+	{
+		C.push_back(rand() % 10 + 1);
+		cout << C[i] << " ";
+	}
 
 	cout << endl;
 
-	checkUnion(A, B, C);
-	checkIntersection(A, B, C);
-	checkResidualAB(A, B, C);
-	checkResidualBA(B, A, C);
+	const int threadNumber = 4;
+	thread* thr[threadNumber];
+
+	thr[0] = new std::thread{ checkUnion, A, B, C };
+	thr[1] = new std::thread{ checkIntersection, A, B, C };
+	thr[2] = new std::thread{ checkResidualAB, A, B, C };
+	thr[3] = new std::thread{ checkResidualBA, B, A, C };
+
+	for (int i = 0; i < threadNumber; i++) {
+		thr[i]->join();
+		delete thr[i];
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
